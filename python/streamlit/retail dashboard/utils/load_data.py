5862b6e7f6542348bcc76995f4f2fd179acf8a39
Uditeshjha <jhauditesh@gmail.com>
2025-09-17 12:25:27 +0530

    streamlit practice on retail dashboard

---
+++ python/streamlit/retail dashboard/utils/load_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import streamlit as st
+
+# df = pd.read_excel('Source Sales Data.xls')
+# print(df.head())
+
+@st.cache_data
+def load_data(file_path="./Source Sales Data.xls"):
+    df = pd.read_excel(file_path)
+    df['Order Date'] = pd.to_datetime(df['Order Date'])
+    df['Ship Date'] = pd.to_datetime(df['Ship Date'])
+    return df
